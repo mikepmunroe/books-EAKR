ffd621386abe3433e152c6f3dd8661603b178abe
Mike Munroe <mike@mikemunroe.com>
2014-02-09 09:53:10 -0500

    regenerate to fix white screen issues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BooksEakr::Application.config.secret_key_base = 'b16feff000fda496f166b0630696578ce90dd237e1062860f370b3b7d9f0eb2703ca97881d1f54517ca74898668fc1a83daea20fa0501272925a6d3f61f8a73a'
